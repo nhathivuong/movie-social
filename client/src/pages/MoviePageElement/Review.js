01a8def1f2d45b9c5d897b4bf5f6aaa263c05f63
Nha Thi Vuong <nhathivuong@gmail.com>
2025-04-03 13:57:22 -0400

    review css

--- client/src/pages/MoviePageElement/Review.js
+++ client/src/pages/MoviePageElement/Review.js
@@ -1,22 +1,49 @@
+import styled from "styled-components"
+
 const Reviews = ({movieReviews}) => {
     return (
         <div>
             <h2>Reviews</h2>
             {movieReviews.length > 0 
             ?<div>{movieReviews.map((review) => {
-                return <div key={review.id}>
-                    <img src={review.author_details.avatar_path
-                    ? `https://image.tmdb.org/t/p/original${review.author_details.avatar_path}` 
-                    : "/assets/default_picture.svg"} 
-                    alt={`${review.author_details.username} profile picture`} width={100}/>
-                    <h3>{review.author_details.username}</h3>
-                    <p>{review.author_details.rating}</p>
-                    <p>{review.content}</p>
-                </div>
+                return <ReviewBox key={review.id}>
+                    <div>
+                        <ProfilePicture src={review.author_details.avatar_path
+                        ? `https://image.tmdb.org/t/p/original${review.author_details.avatar_path}` 
+                        : "/assets/default_picture.svg"} 
+                        alt={`${review.author_details.username} profile picture`} />
+                        {review.author_details.rating && <p>rating: {review.author_details.rating}</p>}
+                    </div>
+                    <div>
+                        <Username>{review.author_details.username}</Username>
+                        <ReviewText >{review.content}</ReviewText>
+                    </div>
+                </ReviewBox>
             })}</div>
             :<p>No Reviews</p>}
         </div>
     )
 }
-
+const ReviewBox = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin: 2rem 0;
+    padding: 1rem;
+    gap: 1rem;
+    box-shadow: 0 0 3px var(--color-light) inset, 0 0 10px var(--color-dark) inset;
+    border-radius: 10px;
+`
+const ProfilePicture = styled.img`
+    width:70px; 
+    height:70px;
+    border-radius: 50%;
+    object-fit: cover;
+    box-sizing: border-box;
+`
+const Username = styled.h2`
+    font-size: 1.5rem;
+`
+const ReviewText = styled.p`
+    margin-top: 1rem;
+`
 export default Reviews
