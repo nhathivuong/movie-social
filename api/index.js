37810ebe9a28db68c1a45958e4afa68446a24c87
Nha Thi Vuong <nhathivuong@gmail.com>
2025-04-08 09:58:01 -0400

    deploy test port

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
+require("dotenv").config()
+const {BACKEND_URL} = process.env
 
-const PORT = 4000;
+const PORT = BACKEND_URL || 4000;
 
 const app = express();
-
-app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.use(express.json());
 app.use(morgan("tiny"));
