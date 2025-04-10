95d76b5b301ff0e57f3b1dbb1b1584bf8bb02ab5
Nha Thi Vuong <nhathivuong@gmail.com>
2025-04-10 16:21:43 -0400

    search

--- client/src/pages/Search/SearchbyGenre.js
+++ client/src/pages/Search/SearchbyGenre.js
@@ -1,19 +1,33 @@
 import { useEffect, useState } from "react"
+import styled from "styled-components";
+
+//icons
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
+
+//component
 import MoviePoster from "../MoviePoster";
 
 // this gets the movies sorted by popularity based on genre
 const SearchbyGenre = ({genreId}) => {
     const [moviesbyGenre , setMoviesbyGenre] = useState()
+    //this page number will be the one used in the backend
+    const [pageNumber, setPageNumber] = useState(1)
+    //this takes the maximum number of page or the results
+    const [maxPage, setMaxPage] = useState()
 
     useEffect(()=>{
         //this ensure the moviesbyGenre is empty each time we switch
         setMoviesbyGenre()
         const fetchMovies = async() => {
             try {
-                const response = await fetch(`https://movie-social.onrender.com/api/genre/${genreId}`);
+                //this ensure the moviesbyGenre is empty each time we switch genre or pageNumber
+                setMoviesbyGenre()
+                const response = await fetch(`https://movie-social.onrender.com/api/genre/${genreId}?page=${pageNumber}`);
                 const data = await response.json();
                 if (data.status === 200) {
                     setMoviesbyGenre(data.moviesByGenre);
+                    setMaxPage(data.maxPages)
                 }
             }
             catch(error){
@@ -21,15 +35,105 @@ const SearchbyGenre = ({genreId}) => {
             }
         }
         fetchMovies()
-    },[genreId])
+    },[genreId, pageNumber])
 
+    const increasePage = (event) =>{
+        event.preventDefault()
+        if(pageNumber < maxPage){
+            setPageNumber((num) => num + 2)
+        }
+    }
+    const decreasePage = (event) =>{
+        event.preventDefault()
+        if(pageNumber > 1){
+            setPageNumber((num) => num - 2)
+        }
+    }
+    const handleToTop = () =>{
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    }
     return <>
+        <PageSection>
+            <Arrows onClick={decreasePage}><IoIosArrowBack/></Arrows>
+            <PageNumber>Page {(pageNumber + 1) / 2} out of {Math.floor((maxPage)/2)}</PageNumber>
+            <Arrows onClick={increasePage}><IoIosArrowForward/></Arrows>
+        </PageSection>
+        <MoviesGrid>
         {moviesbyGenre 
         ? moviesbyGenre.map((movie)=>{
             return <MoviePoster key={movie.id} movie={movie}/>
         })
-        :<p>Loading...</p> }</>
+        :<p>Loading...</p> }
+        </MoviesGrid>
+        <BottomPageNumber>
+            <PageSectionBottom>
+                <Arrows onClick={decreasePage}><IoIosArrowBack/></Arrows>
+                <PageNumber>Page {(pageNumber + 1) / 2} out of {Math.floor((maxPage)/2)}</PageNumber>
+                <Arrows onClick={increasePage}><IoIosArrowForward/></Arrows>
+            </PageSectionBottom>
+            <BackToTop onClick={handleToTop}>Back to top</BackToTop>    
+        </BottomPageNumber>
+    </>
 }
-
-
+const PageSection = styled.div`
+    width:fit-content;
+    margin: auto;
+    display:flex;
+    flex-direction: row;
+`
+const Arrows = styled.button`
+    margin: 0 8px;
+    padding: 5px;
+    cursor: pointer;
+    height: fit-content;
+    background:none;
+    color: var(--color-accent);
+    border:none;
+    border-radius: 5px;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    &:hover{
+        outline: 2px solid var(--color-accent);
+    }
+    &:active{
+        outline: 1px solid var(--color-accent);
+        box-shadow: 0px 0px 2px var(--color-accent) inset;
+    }
+`
+const PageNumber = styled.p`
+    color:var(--color-light);
+`
+const MoviesGrid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(8, 155px);
+    gap: 15px;
+    height:fit-content;
+    width: fit-content;
+    margin: 1rem auto;
+    justify-content: center;
+`
+const BottomPageNumber = styled.div`
+    width:fit-content;
+    margin: auto;
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+`
+const BackToTop = styled.button`
+    background-color: transparent;
+    border: none;
+    color:var(--color-accent);
+    margin-bottom:1rem;
+    cursor: pointer;
+    &:hover{
+        text-decoration: underline;
+    }
+`
+const PageSectionBottom = styled(PageSection)`
+    padding: 1rem;
+`
 export default SearchbyGenre
--- client/src/pages/Search/SearchedMovies.js
+++ client/src/pages/Search/SearchedMovies.js
@@ -1,17 +1,32 @@
 import { useEffect, useState } from "react"
+import styled from "styled-components";
+
+//icons
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
+
+//component
 import MoviePoster from "../MoviePoster"
 
 const SearchedMovies = ({search}) => {
     const [moviesSearched, setMoviesSearched] = useState()
+    //this page number will be the one used in the backend
+    const [pageNumber, setPageNumber] = useState(1)
+    //this takes the maximum number of page or the results
+    const [maxPage, setMaxPage] = useState()
+
     useEffect(()=>{
         //this ensure the moviesSearched is empty each time we switch
         setMoviesSearched()
         const fetchMovies = async() => {
+            //this double ensure the moviesSearched is empty each time we switch
+            setMoviesSearched()
             try {
-                const response = await fetch(`https://movie-social.onrender.com/api/search/${search}`);
+                const response = await fetch(`https://movie-social.onrender.com/api/search/${search}?page=${pageNumber}`);
                 const data = await response.json();
                 if (data.status === 200) {
                     setMoviesSearched(data.moviesSearched);
+                    setMaxPage(data.maxPages)
                 }
             }
             catch(error){
@@ -19,14 +34,104 @@ const SearchedMovies = ({search}) => {
             }
         }
         fetchMovies()
-    },[search])
-
+    },[search, pageNumber])
+    const increasePage = (event) =>{
+        event.preventDefault()
+        if(pageNumber < maxPage){
+            setPageNumber((num) => num + 2)
+        }
+    }
+    const decreasePage = (event) =>{
+        event.preventDefault()
+        if(pageNumber > 1){
+            setPageNumber((num) => num - 2)
+        }
+    }
+    const handleToTop = () =>{
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    }
     return <>
-    {moviesSearched 
-    ? moviesSearched.map((movie)=>{
-        return <MoviePoster key={movie.id} movie={movie}/>
-    })
-    :<p>Loading...</p> }</>
+        <PageSection>
+            <Arrows onClick={decreasePage}><IoIosArrowBack/></Arrows>
+            <PageNumber>Page {(pageNumber + 1) / 2} out of {Math.floor((maxPage)/2)}</PageNumber>
+            <Arrows onClick={increasePage}><IoIosArrowForward/></Arrows>
+        </PageSection>
+        <MoviesGrid>
+        {moviesSearched 
+        ? moviesSearched.map((movie)=>{
+            return <MoviePoster key={movie.id} movie={movie}/>
+        })
+        :<p>Loading...</p> }
+        </MoviesGrid>
+        <BottomPageNumber>
+            <PageSectionBottom>
+                <Arrows onClick={decreasePage}><IoIosArrowBack/></Arrows>
+                <PageNumber>Page {(pageNumber + 1) / 2} out of {Math.floor((maxPage)/2)}</PageNumber>
+                <Arrows onClick={increasePage}><IoIosArrowForward/></Arrows>
+            </PageSectionBottom>
+            <BackToTop onClick={handleToTop}>Back to top</BackToTop>    
+        </BottomPageNumber>
+    </>
 }
-
+const PageSection = styled.div`
+    width:fit-content;
+    margin: auto;
+    display:flex;
+    flex-direction: row;
+`
+const Arrows = styled.button`
+    margin: 0 8px;
+    padding: 5px;
+    cursor: pointer;
+    height: fit-content;
+    background:none;
+    color: var(--color-accent);
+    border:none;
+    border-radius: 5px;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    &:hover{
+        outline: 2px solid var(--color-accent);
+    }
+    &:active{
+        outline: 1px solid var(--color-accent);
+        box-shadow: 0px 0px 2px var(--color-accent) inset;
+    }
+`
+const PageNumber = styled.p`
+    color:var(--color-light);
+`
+const MoviesGrid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(8, 155px);
+    gap: 15px;
+    height:fit-content;
+    width: fit-content;
+    margin: 1rem auto;
+    justify-content: center;
+`
+const BottomPageNumber = styled.div`
+    width:fit-content;
+    margin: auto;
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+`
+const BackToTop = styled.button`
+    background-color: transparent;
+    border: none;
+    color:var(--color-accent);
+    margin-bottom:1rem;
+    cursor: pointer;
+    &:hover{
+        text-decoration: underline;
+    }
+`
+const PageSectionBottom = styled(PageSection)`
+    padding: 1rem;
+`
 export default SearchedMovies
