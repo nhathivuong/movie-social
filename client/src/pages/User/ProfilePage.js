89ab58647e6fe1d441b0dc45a28e3cab9810340c
Nha Thi Vuong <nhathivuong@gmail.com>
2025-04-10 23:23:33 -0400

    save

--- client/src/pages/User/ProfilePage.js
+++ client/src/pages/User/ProfilePage.js
@@ -13,12 +13,13 @@ import UserReview from "./UserReview";
 import UserList from "./UserList";
 
 const ProfilePage = () =>{
-    const { loggedInUser} = useContext(UserContext)
+    const { loggedInUser, setUpdateUser} = useContext(UserContext)
     const {allReviews} = useContext(AllReviewsContext)
     const {username} = useParams()
     const [userInfos, setUserInfos] = useState()
     const [movies, setMovies] = useState()
     const [userReviews, setUserReviews] = useState()
+    const [feedBackMessage, setFeedBackMessage] = useState()
 
     useEffect(()=>{
         const loadData = async () => {
@@ -51,11 +52,72 @@ const ProfilePage = () =>{
     if(!allReviews ||!userInfos){
         return <p>Loading profile</p>
     }
-    
+    /////////////////////////////
+    //handle Profile edit
+    const handleEdit = (event) =>{
+        event.preventDefault()
+        const body = JSON.stringify({
+            username: loggedInUser.username,
+            rating : rating,
+            content: document.getElementById("review").value
+        }) 
+        const options = {
+            method: "POST",
+            headers: {
+                "Accept" : "application/json",
+                "Content-Type" : "application/json",
+            },
+            body
+        }
+        fetch(`https://movie-social.onrender.com/user/:username`, options)
+        .then(res => {
+            if(!res.ok){
+                throw new Error("The profile was not updated")
+            }
+            return res.json()
+        })
+        .then(data => {
+            if(data.status === 201){
+                setFeedBackMessage(data.message)
+                setTimeout(()=>{
+                    setReviewVisible(false)
+                    setFeedBackMessage()
+                    setUpdateUser(update => update+1)
+                },3000)
+            }
+        })
+        .catch(error => console.error(error.message))
+    }
+    //changes the visibility of the form 
+    const toggleVisibility = () =>{
+        if(!loggedInUser){
+            navigate("/logIn")
+        }
+        setReviewVisible(!reviewVisible)
+        setListVisible(false)
+    }
+    const handleRating = (event) =>{
+        setRating(Number(event.target.value))
+    }
     return <ProfileSection>
         <Profile>
             <Picture src={userInfos.src} alt={`${userInfos.name}'s profile picture`}/>
-            <p>Edit profile</p>
+            {userInfos.username === loggedInUser.username && <Edit>Edit profile</Edit>}
+            {editProfileVisible && 
+            <ReviewForm>
+                <Title>
+                    <h2>Review</h2>
+                    <ClosingButton type="button" onClick={toggleVisibility}>x</ClosingButton>
+                </Title>
+                <form onSubmit={handleEdit}>
+                    <label htmlFor="pronoun">Pronouns</label>
+                    <input id="pronoun" name="pronoun" type="text"  placeholder="They/Them"/><br/>
+                    <label htmlFor="bio">Write a bio</label><br/>
+                    <ReviewWritting id="bio" name="bio" type="bio"  placeholder="Write your bio here"/><br/>
+                    <Button type="submit">Send</Button>
+                    {feedBackMessage && <p>{feedBackMessage}</p>}
+                </form>
+            </ReviewForm>}
             <NameAlign>
                 <h2>{userInfos.name}</h2>
                 <p>{userInfos.pronouns}</p>
@@ -100,6 +162,26 @@ const Picture = styled.img`
     border-radius:50%;
     border: 2px solid var(--color-dark-accent);
     box-shadow: 0 0 3px var(--color-dark-accent);
+`
+const Edit = styled.button`
+    margin: auto;
+    padding: 5px;
+    cursor: pointer;
+    height: fit-content;
+    background:none;
+    color: var(--color-dark);
+    border:none;
+    border-radius: 5px;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    &:hover{
+        text-decoration:underline;
+    }
+    &:active{
+        outline: 1px solid var(--color-light);
+        box-shadow: 0px 0px 2px var(--color-dark) inset;
+    }
 `
 const NameAlign = styled.div`
     display:flex;
