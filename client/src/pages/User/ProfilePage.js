0c6b09c1ba3c06888cc5f34d577c128f0bd8807c
Nha Thi Vuong <nhathivuong@gmail.com>
2025-04-11 18:41:58 -0400

    edit profile

--- client/src/pages/User/ProfilePage.js
+++ client/src/pages/User/ProfilePage.js
@@ -11,15 +11,15 @@ import AddFriend from "./AddFriend";
 import RemoveFriend from "./RemoveFriend";
 import UserReview from "./UserReview";
 import UserList from "./UserList";
+import EditProfile from "./EditProfile";
 
 const ProfilePage = () =>{
-    const { loggedInUser, setUpdateUser} = useContext(UserContext)
+    const { loggedInUser} = useContext(UserContext)
     const {allReviews} = useContext(AllReviewsContext)
     const {username} = useParams()
     const [userInfos, setUserInfos] = useState()
     const [movies, setMovies] = useState()
     const [userReviews, setUserReviews] = useState()
-    const [feedBackMessage, setFeedBackMessage] = useState()
 
     useEffect(()=>{
         const loadData = async () => {
@@ -52,77 +52,16 @@ const ProfilePage = () =>{
     if(!allReviews ||!userInfos){
         return <p>Loading profile</p>
     }
-    /////////////////////////////
-    //handle Profile edit
-    const handleEdit = (event) =>{
-        event.preventDefault()
-        const body = JSON.stringify({
-            username: loggedInUser.username,
-            rating : rating,
-            content: document.getElementById("review").value
-        }) 
-        const options = {
-            method: "POST",
-            headers: {
-                "Accept" : "application/json",
-                "Content-Type" : "application/json",
-            },
-            body
-        }
-        fetch(`https://movie-social.onrender.com/user/:username`, options)
-        .then(res => {
-            if(!res.ok){
-                throw new Error("The profile was not updated")
-            }
-            return res.json()
-        })
-        .then(data => {
-            if(data.status === 201){
-                setFeedBackMessage(data.message)
-                setTimeout(()=>{
-                    setReviewVisible(false)
-                    setFeedBackMessage()
-                    setUpdateUser(update => update+1)
-                },3000)
-            }
-        })
-        .catch(error => console.error(error.message))
-    }
-    //changes the visibility of the form 
-    const toggleVisibility = () =>{
-        if(!loggedInUser){
-            navigate("/logIn")
-        }
-        setReviewVisible(!reviewVisible)
-        setListVisible(false)
-    }
-    const handleRating = (event) =>{
-        setRating(Number(event.target.value))
-    }
+
     return <ProfileSection>
         <Profile>
             <Picture src={userInfos.src} alt={`${userInfos.name}'s profile picture`}/>
-            {userInfos.username === loggedInUser.username && <Edit>Edit profile</Edit>}
-            {editProfileVisible && 
-            <ReviewForm>
-                <Title>
-                    <h2>Review</h2>
-                    <ClosingButton type="button" onClick={toggleVisibility}>x</ClosingButton>
-                </Title>
-                <form onSubmit={handleEdit}>
-                    <label htmlFor="pronoun">Pronouns</label>
-                    <input id="pronoun" name="pronoun" type="text"  placeholder="They/Them"/><br/>
-                    <label htmlFor="bio">Write a bio</label><br/>
-                    <ReviewWritting id="bio" name="bio" type="bio"  placeholder="Write your bio here"/><br/>
-                    <Button type="submit">Send</Button>
-                    {feedBackMessage && <p>{feedBackMessage}</p>}
-                </form>
-            </ReviewForm>}
+            <EditProfile userInfos={userInfos} username={username}/>
             <NameAlign>
                 <h2>{userInfos.name}</h2>
-                <p>{userInfos.pronouns}</p>
                 <p>@{userInfos.username}</p>
             </NameAlign>
+            <PronounsCapitalize>{userInfos.pronouns}</PronounsCapitalize>
             <BioText>{userInfos.bio}</BioText>
             {loggedInUser && userInfos.username !== loggedInUser.username && !loggedInUser.follows.includes(userInfos.username) && <AddFriend currentUser={loggedInUser.username} followUser={userInfos.username}/>}
             {loggedInUser && userInfos.username !== loggedInUser.username && loggedInUser.follows.includes(userInfos.username) && <RemoveFriend currentUser={loggedInUser.username} unfollowUser={userInfos.username}/>}
@@ -144,7 +83,7 @@ const ListsReviews = styled.div`
 `
 const Profile = styled.div`
     position: fixed;
-    width: 19%;
+    width: 19vw;
     height: fit-content;
     display:flex;
     flex-direction: column;
@@ -163,31 +102,14 @@ const Picture = styled.img`
     border: 2px solid var(--color-dark-accent);
     box-shadow: 0 0 3px var(--color-dark-accent);
 `
-const Edit = styled.button`
-    margin: auto;
-    padding: 5px;
-    cursor: pointer;
-    height: fit-content;
-    background:none;
-    color: var(--color-dark);
-    border:none;
-    border-radius: 5px;
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    &:hover{
-        text-decoration:underline;
-    }
-    &:active{
-        outline: 1px solid var(--color-light);
-        box-shadow: 0px 0px 2px var(--color-dark) inset;
-    }
-`
 const NameAlign = styled.div`
     display:flex;
     flex-direction:row;
     align-items:baseline;
     gap: 8px;
+`
+const PronounsCapitalize = styled.p`
+    text-transform:capitalize;
 `
 const BioText = styled.p`
     margin-top: 0.5rem;
